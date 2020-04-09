3f48c79838d55fdba1b66b982720031f6b3d9aad
Zinnso1dat <63222437+Zinnso1dat@users.noreply.github.com>
2020-04-09 11:28:17 +0400

    Add files via upload

---
+++ Program123.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Moving += student_Moving;
+
+            student.Move(7);
+        }
+        static void student_Moving(string messege)
+        {
+            Console.WriteLine(messege);
+        }
+    }
+    public delegate void ShowMessege(string messege);
+    public class Student
+    {
+        public void Move(int distance)
+        {
+            for (int i = 1; i <= distance; i++)
+            {
+                Thread.Sleep(1000);
+                if (Moving != null)
+                    Moving(string.Format("Идет перемещение... пройдено километров:  {0}", i));
+            }
+        }
+        public event Action<string> Moving;
+    }
+}
